bca1b41ce0407da18afa3278be17d904be11b5ea
sur0g <sur0gg@gmail.com>
2018-12-20 09:57:12 +0200

    Added utils

--- main.py
+++ main.py
@@ -8,17 +8,6 @@ from jira import JIRA
 from urllib3.util import Url
 
 
-def lazy_property(fn):
-    attr_name = '_lazy_' + fn.__name__
-
-    @property
-    def _lazy_property(self):
-        if not hasattr(self, attr_name):
-            setattr(self, attr_name, fn(self))
-        return getattr(self, attr_name)
-    return _lazy_property
-
-
 class Config:
 
     def __init__(self):
---
+++ utils.py
@@ -0,0 +1,21 @@
+
+class SingletonDecorator:
+    def __init__(self ,klass):
+        self.klass = klass
+        self.instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self.instance is None:
+            self.instance = self.klass(*args ,**kwargs)
+        return self.instance
+
+
+def lazy_property(fn):
+    attr_name = '_lazy_' + fn.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+    return _lazy_property
